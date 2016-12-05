d2af653e3aa1bd14d2b7d2f650d648d75e7e9511
Charlie Reynolds <reynoldscem@gmail.com>
2016-12-05 08:44:07 +0000

    Bit of cleaning

--- 05/1.py
+++ 05/1.py
@@ -1,6 +1,8 @@
+from itertools import count
 import argparse
 import hashlib
 import os
+
 
 def build_parser():
     parser = argparse.ArgumentParser()
@@ -9,8 +11,7 @@ def build_parser():
 
 
 def keygen(data):
-    i = 0
-    while True:
+    for i in count():
         m = hashlib.md5()
         m.update(
             '{}{}'.format(data, i).encode('utf-8')
@@ -18,7 +19,7 @@ def keygen(data):
         hash_val = m.hexdigest()
         if hash_val.startswith('0' * 5):
             yield hash_val[5]
-        i += 1
+
 
 def main(args):
     with open(args.filename) as fd:
@@ -29,7 +30,6 @@ def main(args):
     password = ''.join(next(gen) for _ in range(8))
 
     print(password)
-
 
 
 if __name__ == '__main__':
