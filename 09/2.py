2c0caa5144563a8e123d8a00d7cfe0d02cd288f2
Charles Reynolds <reynoldscem@gmail.com>
2016-12-10 15:29:39 +0000

    Day 9 part 2

--- 09/2.py
+++ 09/2.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
+from functools import lru_cache
 import numpy as np
 import argparse
 import time
+import sys
 import os
 import re
 
@@ -16,30 +18,31 @@ def build_parser():
     return parser
 
 
-def main(args):
-    with open(args.filename) as fd:
-        data = fd.read().rstrip()
-
+@lru_cache(maxsize=None)
+def get_string_len(data):
     pattern = re.compile(r'\(\d+x\d+\)')
     digits = re.compile(r'\d+')
     paren = re.compile(r'^[^\(]+')
 
-    while True:
-        m = pattern.search(data)
-        if m is None:
-            break
-        pre_match = data[:m.start()]
-        post_match = data[m.end():]
-        specifier = data[m.start():m.end()]
-        subseq_len, repeat_num = map(int, re.findall(digits, specifier))
+    m = pattern.search(data)
+    if m is None:
+        return len(data)
+    pre_match = data[:m.start()]
+    post_match = data[m.end():]
+    specifier = data[m.start():m.end()]
+    subseq_len, repeat_num = map(int, re.findall(digits, specifier))
 
-        subseq = post_match[0:subseq_len]
-        post_seq = post_match[subseq_len:]
-        res = subseq * repeat_num
+    subseq = post_match[0:subseq_len]
+    post_seq = post_match[subseq_len:]
+    res = subseq * repeat_num
 
-        data = pre_match + res + post_seq
+    return len(pre_match) + get_string_len(res) + get_string_len(post_seq)
 
-    print(len(data))
+def main(args):
+    with open(args.filename) as fd:
+        data = fd.read().rstrip()
+    string_len = get_string_len(data)
+    print(string_len)
 
 
 if __name__ == '__main__':
