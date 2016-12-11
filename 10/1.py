a39856030be1cfa8b906d0b36c4957685d5a446e
Charles Reynolds <reynoldscem@gmail.com>
2016-12-11 00:02:03 +0000

    changes

--- 10/1.py
+++ 10/1.py
@@ -1,58 +1,99 @@
 from multiprocessing import Process, Manager
-import sys
+import argparse
+import os
 import re
 
 class Bot:
-    def __init__(self, init_string, registry, m, maxsize=2, target=[17, 61]):
+    def __init__(self, init_string, registry, m, maxsize=2, target=[2, 5]):
         self.name, self.low_dest, self.high_dest = re.findall(
             r'((?:bot|output) \d+)',
             init_string
         )
+
+        registry[self.name] = self
         self.registry = registry
-        self.registry[self.name] = self
         self.queue = m.Queue(maxsize=maxsize)
         self.target = target
 
-    def transfer_chips(self):
-        print('yo')
+    def transfer_chips(self, registry):
         items = [self.queue.get() for _ in range(self.queue.qsize())]
 
         if items == self.target:
             print(self.name)
+            return True
 
-        print(self.registry[self.low_dest])
-        self.registry[self.low_dest].put(items[0])
-        self.registry[self.high_dest].put(items[1])
+        print(registry)
+        registry[self.low_dest].queue.put(items[0])
+        registry[self.high_dest].queue.put(items[1])
 
-    def work(self):
-        print(self)
-        while True:
+    def work(self, registry):
+        print('Work')
+        print(registry)
+        found = False
+        while not found:
             if self.queue.full():
-                print('Blah')
-                sys.stdout.flush()
-                self.transfer_chips()
+                found = self.transfer_chips(registry)
 
 
 class OutBin:
     def __init__(self, m):
         self.queue = m.Queue()
+        self.registry = None
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('filename')
+
+    return parser
+
+
+def main(args):
+    assert os.path.isfile(args.filename), 'File must exist.'
+    with open(args.filename) as fd:
+        data = fd.read().splitlines()
+
+    bot_def_lines = [
+        line for line in data
+        if line.startswith('bot')
+    ]
+
+    m = Manager()
+    registry = dict()
+    for bot_def_line in bot_def_lines:
+        outputs = map(
+            int,
+            re.findall(
+                r'(?:output (\d+))',
+                bot_def_line
+            )
+        )
+
+        for out_idx in outputs:
+            string = 'output {}'.format(out_idx)
+            if string not in registry.keys():
+                registry[string] = OutBin(m)
+
+        bot = Bot(bot_def_line, registry, m)
+        p = Process(target=bot.work, args=(registry,))
+        p.start()
+
+    for bot in registry.values():
+        print(bot.registry)
+    print(registry)
+    value_lines = [
+        line for line in data
+        if line.startswith('value')
+    ]
+
+    for line in value_lines:
+        value, bot_id = re.findall(r'\d+', line)
+        registry['bot {}'.format(bot_id)].queue.put(value)
+
+    p.join()
 
 
 if __name__ == '__main__':
-    m = Manager()
-    registry = m.dict()
-    registry['output 0'] = OutBin(m) # OutBin(m)
-    registry['output 1'] = OutBin(m)
-    # registry['output 1'] = m.Queue() # OutBin(m)
-    print(registry['output 1'])
-    # bot = Bot('bot 2 gives low to output 0 and high to output 1', registry, m)
-
-
-    # bot.queue.put(17)
-    # bot.queue.put(61)
-    # p = Process(target=bot.work)
-    # p.start()
-    # print('Here')
-    # print(bot.queue)
-    # sys.stdout.flush()
-    # p.join()
+    args = build_parser().parse_args()
+    main(args)
